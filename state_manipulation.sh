70911a084fc38ff8ea913e805a510bb359286518
Nicholas Drone <nicholas.drone@kroger.com>
2023-11-09 12:20:30 -0500

    switches bash interpreter

--- state_manipulation.sh
+++ state_manipulation.sh
@@ -1,5 +1,9 @@
-#!/usr/bin/env bash -e
+#!/usr/bin/bash
 
 # This script is what we are executing our terraform commands to manipulate state.
+
+# So that when a command fails the script exists instead of continuing with the
+# rest of the script.
+set -o errexit
 
 terraform -v
