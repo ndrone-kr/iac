5c1af7b9bc61e2bc704cdf1c4c60308204c71ff9
Nicholas Drone <nicholas.drone@kroger.com>
2023-11-09 13:10:33 -0500

    Adds init and state backup commands

--- state_manipulation.sh
+++ state_manipulation.sh
@@ -2,8 +2,18 @@
 
 # This script is what we are executing our terraform commands to manipulate state.
 
-# So that when a command fails the script exists instead of continuing with the
-# rest of the script.
+# So that when a command fails the script exists instead of continuing with the rest of the script. Now that we have 
+# a backup of the state we may want to push the orginal state on failure. But this may change based on use case. In
+# my opinon I would prefer to exit so that we can review and determine best course of action.
 set -o errexit
 
+# test to ensure we have terraform installed
 terraform -v
+
+# initialize terraform
+terraform init
+
+# create a backup of the existing state file
+# TODO think about pushing this to a storage account instead of keeping it on the local filesystem
+now=`date '+%Y_%m_%d__%H_%M_%S'`;
+terraform state pull >> terraform.tfstate.backup-$now
